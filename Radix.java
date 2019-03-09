8b4197e0d8f8e61e38af0ded3f35a54fb20c9796
SZYiJie <46023505+SZYiJie@users.noreply.github.com>
2019-03-09 21:27:48 +0800

    Add files via upload

---
+++ Radix.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Radix{
+	public static void main(String[] args) {
+		int a = 11; 			// 十进制整型
+		System.out.println(a);	// 输出十进制表示的整型值
+		int b = 011; 			// 八进制整型
+		System.out.println(b); 	// 输出八进制表示的整型值
+		int c = 0x11; 			// 十六进制整型
+		System.out.println(c);	// 输出十六进制表示的整型值
+	}
+}
